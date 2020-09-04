d6b3355d94291b90d2d98b9bbcddd18021331402
Pratik282001 <pratikphadtare109@gmail.com>
2020-09-04 20:45:50 +0530

    added new api key

--- Desktop/Newspaper/newspaper.py
+++ Desktop/Newspaper/newspaper.py
@@ -8,7 +8,10 @@ def speak(str):
     speak.Speak(str)
 
 if __name__ == '__main__':
+    # in place API_KEY you can use your own api keys from website newsapi.org ;)
+    # one example is given below
     response = requests.get("http://newsapi.org/v2/top-headlines?country=in&apiKey=c546cd7dd0e4454d81e3d3b684eff6bf")
+    # response = requests.get("http://newsapi.org/v2/everything?q=apple&from=2020-09-03&to=2020-09-03&sortBy=popularity&apiKey=API_KEY")
     text = response.text
     my_json = json.loads(text)
     speak("    today's top headlines are")
